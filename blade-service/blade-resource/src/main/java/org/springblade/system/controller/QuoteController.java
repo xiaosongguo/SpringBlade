9266fd516d35b06e9d2d8b559c8f7823a8c7e486
songls <970458312@qq.com>
2019-06-19 11:28:41 +0800

    上传路径回传

--- blade-service/blade-resource/src/main/java/org/springblade/system/controller/QuoteController.java
+++ blade-service/blade-resource/src/main/java/org/springblade/system/controller/QuoteController.java
@@ -141,10 +141,10 @@ public class QuoteController extends BladeController {
 	@PostMapping("/upload")
 	@ApiOperation(value = "上传", notes = "", position = 8)
 	public R upload(@RequestParam("file") List<MultipartFile> files, String dir, String path, String virtualPath) {
-		List<BladeFile> errfiles = getFiles(files, dir, path, virtualPath).stream().filter(bladeFile ->
-			!bladeFile.transfer(false)
+		List<BladeFile> succfiles = getFiles(files, dir, path, virtualPath).stream().filter(bladeFile ->
+			bladeFile.transfer(false)
 		).collect(Collectors.toList());
-		return R.data(errfiles);
+		return R.data(succfiles);
 	}
 
 	
